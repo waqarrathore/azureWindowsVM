4adb921002efdcf7f668f81c6ae182c671a90c2a
waqarrathore <waqar.rathore@gmail.com>
2024-01-18 17:07:43 +0000

    create a windows vm

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "resource_group_location" {
+  default     = "UK South"
+  description = "Location of the resource group."
+}
+
+variable "prefix" {
+  type        = string
+  default     = "win-vm-iis"
+  description = "Prefix of the resource name"
+}
