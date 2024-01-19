baa8bdb13401429e3e767a27237338d2091b07bd
waqarrathore <waqar.rathore@gmail.com>
2024-01-19 15:30:19 +0000

    updated variables.tf and modified the main.tf to reflect variables.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,16 @@
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = "${random_pet.prefix.id}-rg"
+  name     = var.resource_group_name
 }
-
-# Create virtual network
-resource "azurerm_virtual_network" "my_terraform_network" {
-  name                = "${random_pet.prefix.id}-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# refer to a subnet
+data "azurerm_subnet" "tfsubnet" {
+  name                 = var.subnet_name
+  virtual_network_name = var.vnet_name
+  resource_group_name  = var.vnet_res_group_name
 }
-
-# Create subnet
-resource "azurerm_subnet" "my_terraform_subnet" {
-  name                 = "${random_pet.prefix.id}-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Create public IPs
-resource "azurerm_public_ip" "my_terraform_public_ip" {
-  name                = "${random_pet.prefix.id}-public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-}
-
 # Create Network Security Group and rules
 resource "azurerm_network_security_group" "my_terraform_nsg" {
-  name                = "${random_pet.prefix.id}-nsg"
+  name                = "${var.virtual_machine_name}-net-sec-grp-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -44,30 +25,18 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-  security_rule {
-    name                       = "web"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
 }
 
 # Create network interface
 resource "azurerm_network_interface" "my_terraform_nic" {
-  name                = "${random_pet.prefix.id}-nic"
+  name                = "${var.virtual_machine_name}-nic"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "my_nic_configuration"
-    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+    subnet_id                     = data.azurerm_subnet.tfsubnet.id
+    private_ip_address_allocation = var.private_ip_address
   }
 }
 
@@ -89,7 +58,7 @@ resource "azurerm_storage_account" "my_storage_account" {
 
 # Create virtual machine
 resource "azurerm_windows_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
+  name                  = var.virtual_machine_name
   admin_username        = "azureuser"
   admin_password        = random_password.password.result
   location              = azurerm_resource_group.rg.location
--- variables.tf
+++ variables.tf
@@ -3,6 +3,35 @@ variable "resource_group_location" {
   description = "Location of the resource group."
 }
 
+variable "resource_group_name" {
+  default     = "virt-mach-rg"
+  description = "Name ofthe resource group"
+}
+
+variable "private_ip_address" {
+  default     = "Dynamic"
+  description = "Private IP address configuration"
+}
+
+variable "virtual_machine_name" {
+  default     = "my-test-vm"
+  description = "variable that contains the virtual machine name"
+}
+
+variable "subnet_name" {
+  default     = "subnet10-0-1-0-m24"
+  description = "insert-name-of-your-pre-existing-subnet-here"
+}
+
+variable "vnet_name" {
+  default     = "vnet10-0-0-0-m8"
+  description = "insert-name-of-your-pre-existing-vnet-here"
+}
+variable "vnet_res_group_name" {
+  default     = "vnet-rg"
+  description = "resource group that contains the vnet/subnet"
+}
+
 variable "prefix" {
   type        = string
   default     = "win-vm-iis"
